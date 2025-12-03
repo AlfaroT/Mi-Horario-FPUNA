d44176f0898496d6bbc8069059f99ca327068455
AlfaroT <AlfaroT@users.noreply.github.com>
2025-12-03 16:53:48 +0000

    deploy: ecd61a50a96ca028186327171d8dcbeb2156a3f0

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker para Mi Horario FPUNA
-// Versión: 1.3.2 - Tutorial de instalación
+// Versión: 1.3.3 - Fix button text color
 
-const CACHE_NAME = 'mi-horario-fpuna-v1.3.2';
+const CACHE_NAME = 'mi-horario-fpuna-v1.3.3';
 
 // Detectar si estamos en GitHub Pages o localhost
 const isGitHubPages = self.location.hostname.includes('github.io');
